c6aaccd9780320830bc59ec799d54675e5215eaf
David Tarrant <davetaz@theodi.org>
2023-07-07 12:04:42 +0100

    Adding assessibility for external links

--- js/theme.js
+++ js/theme.js
@@ -1,38 +1,57 @@
 define([
-  "core/js/adapt",
-  "./themePageView",
-  "./themeArticleView",
-  "./themeBlockView",
-  "./themeView"
+  'core/js/adapt',
+  './themePageView',
+  './themeArticleView',
+  './themeBlockView',
+  './themeView'
 ], function(Adapt, ThemePageView, ThemeArticleView, ThemeBlockView, ThemeView) {
 
   function onDataReady() {
-    $("html").addClass(Adapt.course.get("_courseStyle"));
+    $('html').addClass(Adapt.course.get('_courseStyle'));
   }
 
   function onPostRender(view) {
-    var model = view.model;
-    var theme = model.get("_odi-theme");
+    const model = view.model;
+    const theme = model.get('_odi-theme');
 
     if (!theme) return;
 
-    switch (model.get("_type")) {
-      case "page":
+    switch (model.get('_type')) {
+      case 'page':
         new ThemePageView({ model: new Backbone.Model(theme), el: view.$el });
         break;
-      case "article":
+      case 'article':
         new ThemeArticleView({ model: new Backbone.Model(theme), el: view.$el });
         break;
-      case "block":
+      case 'block':
         new ThemeBlockView({ model: new Backbone.Model(theme), el: view.$el });
         break;
       default:
         new ThemeView({ model: new Backbone.Model(theme), el: view.$el });
     }
+
+    document.querySelectorAll('a[target="_blank"]').forEach(link => {
+      addNoOpener(link);
+      addNewTabMessage(link);
+    });
+
+  }
+
+  function addNoOpener(link) {
+    const linkTypes = (link.getAttribute('rel') || '').split(' ');
+    if (!linkTypes.includes('noopener')) {
+      linkTypes.push('noopener');
+    }
+    link.setAttribute('rel', linkTypes.join(' ').trim());
+  }
+
+  function addNewTabMessage(link) {
+    link.setAttribute('aria-label', link.innerHTML + ' - link opens in a new tab');
+    link.setAttribute('title', link.innerHTML);
   }
 
   Adapt.on({
-    "app:dataReady": onDataReady,
-    "pageView:postRender articleView:postRender blockView:postRender": onPostRender
+    'app:dataReady': onDataReady,
+    'pageView:postRender articleView:postRender blockView:postRender': onPostRender
   });
 });
